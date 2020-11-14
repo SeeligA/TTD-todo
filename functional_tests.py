85d9bfa16c0e3cd75727d92bc9fbf741c8bffc96
SeeligA <as@arndtseelig.com>
2020-11-14 18:01:45 +0100

    Add Test Class for managing functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,28 +1,45 @@
 from selenium import webdriver
-
-browser = webdriver.Chrome()
-# Open browser to visit website
-browser.get('http://localhost:8000')
-# The page title mentions "KERN"
-assert 'KERN' in browser.title
-
-# Enter a to-do item straight away
-
-# When writing "Select VCS" into a text box
-
-# And hitting Enter, the page updates. Now the
-# page shows "1. Select VSC" as an to-do item
-
-# When writing another item ("Install VCS")
-# in the text box below...
-
-# ... the list update again, now showing both items
-
-# Will the site remember those items? THe URL shows
-# a unique URL for the user - some text provides
-# additional details
-
-# WHen visiting the URl, the items are still there.
+import unittest
 
 
-browser.quit()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        """Special method for test prerequisites."""
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        """
+        Special method for clean-up steps.
+
+        Note that this method will run even in case of an exception
+        and unless the exception occurs at self.setUp().
+        """
+        self.browser.quit()
+
+    def test_can_start_and_retrieve_list(self):
+        # User opens browser to visit website
+        self.browser.get("http://localhost:8000")
+        # The page title mentions "KERN"
+        self.assertIn("KERN", self.browser.title)
+        self.fail("Finish the test!")
+        # Enter a to-do item straight away
+
+        # When writing "Select VCS" into a text box
+
+        # And hitting Enter, the page updates. Now the
+        # page shows "1. Select VSC" as an to-do item
+
+        # When writing another item ("Install VCS")
+        # in the text box below...
+
+        # ... the list update again, now showing both items
+
+        # Will the site remember those items? THe URL shows
+        # a unique URL for the user - some text provides
+        # additional details
+
+        # WHen visiting the URl, the items are still there.
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
