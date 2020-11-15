976bd089ad6cdf7d567e6b86d8d421f892910771
SeeligA <as@arndtseelig.com>
2020-11-15 15:22:07 +0100

    Add FT for header and list items

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
 import unittest
 
 
@@ -19,19 +22,33 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_and_retrieve_list(self):
         # User opens browser to visit website
         self.browser.get("http://localhost:8000")
-        # The page title mentions "KERN"
+        # There is a page title and title mentions "To-Do"
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test!")
-        # Enter a to-do item straight away
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
+        # Invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # When writing "Select VCS" into a text box
-
+        inputbox.send_keys('Select VCS')
+        # and hitting ENTER
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Select VCS' for row in rows)
+        )
         # And hitting Enter, the page updates. Now the
         # page shows "1. Select VSC" as an to-do item
 
         # When writing another item ("Install VCS")
         # in the text box below...
-
+        self.fail("Finish the test!")
         # ... the list update again, now showing both items
 
         # Will the site remember those items? THe URL shows
